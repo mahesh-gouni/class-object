e6e0df8fff7d29a1c5667c276201c6043d7b58fb
gouni <gounimahesh9000@gmail.com>
2024-07-19 14:30:49 +0530

    integer add

---
+++ student/Intigeradd.java
@@ -0,0 +1,11 @@
+package student;
+
+public class Intigeradd {
+    public static void main(String[] args) {
+        int num1=77;
+        int num2 =89;
+        int result=num1+num2;
+        System.out.println("result="+result);
+    }
+}
+
